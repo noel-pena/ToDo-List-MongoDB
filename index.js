0f37f0e49c69d6d76dc017f62dd67a05f7c69ba1
noel-pena <azulu88@hotmail.com>
2023-12-13 15:16:16 -0500

    first commit

--- index.js
+++ index.js
@@ -14,15 +14,14 @@ const db = new pg.Client({
 });
 db.connect();
 
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let items
+let items;
 
 //Today
 app.get("/", async (req, res) => {
-  const result = await db.query("SELECT * FROM items ORDER BY id ASC")
+  const result = await db.query("SELECT * FROM items ORDER BY id ASC");
   items = result.rows;
   res.render("index.ejs", {
     listTitle: "Today",
@@ -32,9 +31,7 @@ app.get("/", async (req, res) => {
 
 app.post("/add", async (req, res) => {
   const item = req.body.newItem;
-  const input = await db.query("INSERT INTO items (title) values ($1)",
-  [item]
-  );
+  const input = await db.query("INSERT INTO items (title) values ($1)", [item]);
   items.push({ title: item });
   res.redirect("/");
 });
@@ -43,26 +40,25 @@ app.post("/edit", async (req, res) => {
   const data = await db.query("SELECT * FROM items ORDER BY id ASC");
   const updateTitle = req.body.updatedItemTitle;
   const id = req.body.updatedItemId;
-  const pushUpdate = await db.query("UPDATE items SET title = ($1) WHERE id = ($2)",
-  [updateTitle, id]
+  const pushUpdate = await db.query(
+    "UPDATE items SET title = ($1) WHERE id = ($2)",
+    [updateTitle, id]
   );
-  console.log(updateTitle)
+  console.log(updateTitle);
   res.redirect("/");
 });
 
 app.post("/delete", async (req, res) => {
   const data = await db.query("SELECT * FROM items ORDER BY id ASC");
-  const id = req.body.deleteItemId
-  const pushDelete = await db.query("DELETE FROM items WHERE id=($1)",
-  [id]
-  );
-  console.log(id)
+  const id = req.body.deleteItemId;
+  const pushDelete = await db.query("DELETE FROM items WHERE id=($1)", [id]);
+  console.log(id);
   res.redirect("/");
 });
 
 //Month
 app.get("/month", async (req, res) => {
-  const result = await db.query("SELECT * FROM items_month ORDER BY id ASC")
+  const result = await db.query("SELECT * FROM items_month ORDER BY id ASC");
   items = result.rows;
   res.render("month.ejs", {
     listTitle: "Month",
@@ -72,9 +68,9 @@ app.get("/month", async (req, res) => {
 
 app.post("/add-month", async (req, res) => {
   const item = req.body.newItem;
-  const input = await db.query("INSERT INTO items_month (title) values ($1)",
-  [item]
-  );
+  const input = await db.query("INSERT INTO items_month (title) values ($1)", [
+    item,
+  ]);
   items.push({ title: item });
   res.redirect("/month");
 });
@@ -83,26 +79,27 @@ app.post("/edit-month", async (req, res) => {
   const data = await db.query("SELECT * FROM items_month ORDER BY id ASC");
   const updateTitle = req.body.updatedItemTitle;
   const id = req.body.updatedItemId;
-  const pushUpdate = await db.query("UPDATE items_month SET title = ($1) WHERE id = ($2)",
-  [updateTitle, id]
+  const pushUpdate = await db.query(
+    "UPDATE items_month SET title = ($1) WHERE id = ($2)",
+    [updateTitle, id]
   );
-  console.log(updateTitle)
+  console.log(updateTitle);
   res.redirect("/month");
 });
 
 app.post("/delete-month", async (req, res) => {
   const data = await db.query("SELECT * FROM items_month ORDER BY id ASC");
-  const id = req.body.deleteItemId
-  const pushDelete = await db.query("DELETE FROM items_month WHERE id=($1)",
-  [id]
-  );
-  console.log(id)
+  const id = req.body.deleteItemId;
+  const pushDelete = await db.query("DELETE FROM items_month WHERE id=($1)", [
+    id,
+  ]);
+  console.log(id);
   res.redirect("/month");
 });
 
 //Week
 app.get("/Week", async (req, res) => {
-  const result = await db.query("SELECT * FROM items_week ORDER BY id ASC")
+  const result = await db.query("SELECT * FROM items_week ORDER BY id ASC");
   items = result.rows;
   res.render("week.ejs", {
     listTitle: "Week",
@@ -112,9 +109,9 @@ app.get("/Week", async (req, res) => {
 
 app.post("/add-week", async (req, res) => {
   const item = req.body.newItem;
-  const input = await db.query("INSERT INTO items_week (title) values ($1)",
-  [item]
-  );
+  const input = await db.query("INSERT INTO items_week (title) values ($1)", [
+    item,
+  ]);
   items.push({ title: item });
   res.redirect("/week");
 });
@@ -123,20 +120,21 @@ app.post("/edit-week", async (req, res) => {
   const data = await db.query("SELECT * FROM items_week ORDER BY id ASC");
   const updateTitle = req.body.updatedItemTitle;
   const id = req.body.updatedItemId;
-  const pushUpdate = await db.query("UPDATE items_week SET title = ($1) WHERE id = ($2)",
-  [updateTitle, id]
+  const pushUpdate = await db.query(
+    "UPDATE items_week SET title = ($1) WHERE id = ($2)",
+    [updateTitle, id]
   );
-  console.log(updateTitle)
+  console.log(updateTitle);
   res.redirect("/week");
 });
 
 app.post("/delete-week", async (req, res) => {
   const data = await db.query("SELECT * FROM items_week ORDER BY id ASC");
-  const id = req.body.deleteItemId
-  const pushDelete = await db.query("DELETE FROM items_week WHERE id=($1)",
-  [id]
-  );
-  console.log(id)
+  const id = req.body.deleteItemId;
+  const pushDelete = await db.query("DELETE FROM items_week WHERE id=($1)", [
+    id,
+  ]);
+  console.log(id);
   res.redirect("/week");
 });
 

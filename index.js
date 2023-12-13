ed76b818d5799eb93990302007d3a7a282f9aaa6
noel-pena <azulu88@hotmail.com>
2023-12-13 15:10:21 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,66 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "Permalist",
+  password: "",
+  port: 5432,
+});
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// let items = [{
+//   id: ""
+//   title: ""
+// }];
+
+app.get("/", async (req, res) => {
+  const result = await db.query("SELECT * FROM items ORDER BY id ASC")
+  items = result.rows
+  res.render("index.ejs", {
+    listTitle: "Today",
+    listItems: items,
+  });
+});
+
+app.post("/add", async (req, res) => {
+  const item = req.body.newItem;
+  const input = await db.query("INSERT INTO items (title) values ($1)",
+  [item]
+  );
+  items.push({ title: item });
+  res.redirect("/");
+});
+
+app.post("/edit", async (req, res) => {
+  const data = await db.query("SELECT * FROM items ORDER BY id ASC");
+  const updateTitle = req.body.updatedItemTitle;
+  const id = req.body.updatedItemId;
+  const pushUpdate = await db.query("UPDATE items SET title = ($1) WHERE id = ($2)",
+  [updateTitle, id]
+  );
+  console.log(updateTitle)
+  res.redirect("/");
+});
+
+app.post("/delete", async (req, res) => {
+  const data = await db.query("SELECT * FROM items ORDER BY id ASC");
+  const id = req.body.deleteItemId
+  const pushDelete = await db.query("DELETE FROM items WHERE id=($1)",
+  [id]
+  );
+  console.log(id)
+  res.redirect("/");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});

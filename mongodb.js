e79413da543a565a78e71c7131d279d4034abb14
noel-pena <azulu88@hotmail.com>
2024-03-04 22:00:48 -0500

    removing env need

--- mongodb.js
+++ mongodb.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 
-dotenv.config();
+// dotenv.config();
+const MONGODB_URI =
+  "mongodb+srv://noelpena:1234@cluster0.xbaexmf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-const dbUrl = process.env.MONGODB_URI;
+// const dbUrl = process.env.MONGODB_URI;
 
 const itemSchema = new mongoose.Schema({
   title: { type: String, required: true },
@@ -15,7 +17,7 @@ const Item3 = mongoose.model("Items_month", itemSchema);
 
 async function connectToMongoDB() {
   try {
-    await mongoose.connect(dbUrl);
+    await mongoose.connect(MONGODB_URI);
     console.log("Connected to MongoDB!");
   } catch (error) {
     console.error("MongoDB connection error:", error);
